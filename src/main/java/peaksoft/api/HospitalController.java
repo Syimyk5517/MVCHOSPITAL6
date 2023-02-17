160404f8cfda1891998d4367f761545a87603586
Syimykjumabekov <syjmykzumabekuulu@gmail.com>
2023-02-17 18:52:37 +0600

    git commit

--- src/main/java/peaksoft/api/HospitalController.java
+++ src/main/java/peaksoft/api/HospitalController.java
@@ -2,10 +2,24 @@ package peaksoft.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import peaksoft.models.Hospital;
+import peaksoft.services.HospitalService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/hospitals")
 @RequiredArgsConstructor
 public class HospitalController {
+    private final HospitalService hospitalService;
+
+    @GetMapping
+    String getAll(Model model){
+        List<Hospital> hospitals = hospitalService.getAll();
+        model.addAttribute("hospitals",hospitals);
+        return "hospital/hospitals";
+    }
 }
